966d7091e363c30b05a0b0546f04987f1330e3ec
hatsuki-blue <main@hatsuki.link>
2026-05-11 11:50:50 +0900

    10>16 kadai-finish

--- 0511/decimal-to-hexadecimal.c
+++ 0511/decimal-to-hexadecimal.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-
+	int decimal = 0; //初期化
+	while(1){ //繰り返し処理
+		printf("10進数 > ");
+		scanf("%d", &decimal);
+		printf("16進数 > ");
+		printf("%X\n\n",decimal);
+	}
 	return (0);
 }
 

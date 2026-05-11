84306197ae890349fed17ae79dea3653d0954ff4
hatsuki-blue <main@hatsuki.link>
2026-05-11 12:02:56 +0900

    課題完成。課題用に名称変更

---
+++ 0511/x2d.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int hexadecimal = 0; //初期化
+	while(1){ //繰り返し処理
+		printf("16進数 > ");
+		scanf("%X", &hexadecimal);
+		printf("10進数 > ");
+		printf("%d\n\n",hexadecimal);
+	}
+	return (0);
+}
